414e93b80531642d500050916fad1b10cd884bd0
Daniel Kang <daniel.d.kang@gmail.com>
2023-09-07 21:42:20 +0000

    Add a function to get the cache table name from inputs

--- aidb/utils/constants.py
+++ aidb/utils/constants.py
@@ -7,3 +7,10 @@ The schema of the blob metadata table is as follows:
   blob_key: str
 '''
 BLOB_TABLE_NAMES_TABLE = CONFIG_PREFIX + '_blob_tables'
+
+def cache_table_name_from_inputs(columns: List[str]):
+  cache_table_postfix = ''
+  for idx, column in enumerate(columns):
+    # Special characters are not allowed in table names
+    cache_table_postfix += f'__{idx}_{column}__'
+  return CACHE_PREFIX + cache_table_postfix
