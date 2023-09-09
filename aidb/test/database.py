0f0f56e2e693823e58bcef9a3846bc1cce4c666a
ttt-77 <1057858192@qq.com>
2023-09-09 02:09:41 -0500

    Refactor create table and insert table

--- aidb/test/database.py
+++ aidb/test/database.py
@@ -3,12 +3,15 @@ import asyncio
 import sqlalchemy
 import sqlalchemy.ext.asyncio
 import sqlalchemy.ext.automap
+from sqlalchemy import insert
 
 from sqlalchemy.sql import text
+from sqlalchemy import (Boolean, Column, Float, Integer, MetaData,
+                        Table, Text)
+from sqlalchemy.schema import ForeignKeyConstraint
 
 from aidb.utils.logger import logger
-from collections import defaultdict
-import pandas as pd
+
 
 class Database():
     def __init__(
@@ -66,80 +69,77 @@ class Database():
 
         return engine
 
-
     def create_table_sql(self):
+        type_str_to_sql_type = {
+            'int': Integer,
+            'float': Float,
+            'bool': Boolean,
+            'str': Text,  # TODO: required for mysql?
+        }
+        self._metadata = MetaData()
         query_list = []
         tables = self._config['tables']
         for table_name in tables:
-            sql_list = []
-            col_list = []
-            for col_name, col_type in tables[table_name]['columns'].items():
-                if col_type == 'str':
-                    col_type = 'TEXT'
-                col_type = col_type.upper()
-                col_list.append(f'{col_name} {col_type}')
-            sql_list.append(', '.join(col_list))
+            table = tables[table_name]
+            columns = []
+            fk_constraints = {}
+            for col_name, col_type in table['columns'].items():
+                col_type = type_str_to_sql_type[col_type]
+                is_primary_key = col_name in table['primary_key']
+                if 'foreign_keys' in tables[table_name]:
+                    is_foreign_key = col_name in table['foreign_keys']
+                    if is_foreign_key:
+                        fk_ref_table_name = table['foreign_keys'][col_name].split('.')[0]
+                        if fk_ref_table_name not in fk_constraints:
+                            fk_constraints[fk_ref_table_name] = {'cols': [], 'cols_refs': []}
+                        fk_constraints[fk_ref_table_name]['cols'].append(col_name)
+                        fk_constraints[fk_ref_table_name]['cols_refs'].append(table['foreign_keys'][col_name])
+                columns.append(
+                    Column(
+                        col_name,
+                        col_type,
+                        primary_key=is_primary_key,
+                        autoincrement=False,
+                    ))
+            multi_table_fk_constraints = []
+            for tbl, fk_cons in fk_constraints.items():
+                multi_table_fk_constraints.append(ForeignKeyConstraint(fk_cons['cols'], fk_cons['cols_refs']))
 
-            pks = ', '.join(tables[table_name]['primary_key'])
-            sql_list.append(f'PRIMARY KEY ({pks})')
+            table = Table(table_name, self._metadata, *columns, *multi_table_fk_constraints)
 
-            if 'foreign_keys' in tables[table_name]:
-                fk_list = []
-                refer_table_dict = defaultdict(list)
-                for fk, refer_table_col in tables[table_name]['foreign_keys'].items():
-                    refer_table = refer_table_col.split('.')[0]
-                    refer_col = refer_table_col.split('.')[1]
-                    refer_table_dict[refer_table].append((fk, refer_col))
-
-                for refer_table in refer_table_dict:
-                    fks = ', '.join([x[0] for x in refer_table_dict[refer_table]])
-                    refer_cols = ', '.join([x[1] for x in refer_table_dict[refer_table]])
-                    fk_list.append(f'FOREIGN KEY ({fks}) REFERENCES {refer_table}({refer_cols})')
-                sql_list.append(', '.join(fk_list))
-
-            sql = ', '.join(sql_list)
-
-            drop_query = f'''DROP TABLE IF EXISTS {table_name};'''
-            create_query = f'''CREATE TABLE {table_name} ({sql});'''
-            query_list.insert(0, drop_query)
-            query_list.append(create_query)
-
-        # Create metadata table
-        metadata_table_drop_query = '''DROP TABLE IF EXISTS aidb.__config_blob_tables;'''
-        metadata_table_create_query = '''CREATE TABLE aidb.__config_blob_tables ( table_name TEXT, blob_key TEXT );'''
-        query_list.append(metadata_table_drop_query)
+        # TODO: modify this part, maybe move to yaml file
+        metadata_table_create_query = '''CREATE TABLE __config_blob_tables ( table_name TEXT, blob_key TEXT );'''
         query_list.append(metadata_table_create_query)
         for blob_table in self._config['blob_tables']:
             for blob_key in self._config['blob_tables'][blob_table]:
-                insert_query = f'''INSERT INTO aidb.__config_blob_tables (table_name, blob_key) VALUES ('{blob_table}', '{blob_key}');'''
+                insert_query = f'''INSERT INTO __config_blob_tables (table_name, blob_key) VALUES ('{blob_table}', '{blob_key}');'''
                 query_list.append(insert_query)
-
         return query_list
+
 
     async def _create_table(self):
         query_list = self.create_table_sql()
+        drop = MetaData()
+
         async with self._sql_engine.begin() as conn:
+            await conn.run_sync(drop.reflect)
+            await conn.run_sync(drop.drop_all)
             for query in query_list:
                 await conn.execute(text(query))
+            await conn.run_sync(self._metadata.create_all)
 
     # TODO: Should we support to insert table without some columns?
-    async def insert_value(self, df, table_name):
+    async def insert_table(self, df, table_name):
         def read_cols(conn):
-            metadata = sqlalchemy.MetaData()
-            sqlalchemy_table = sqlalchemy.Table(table_name, metadata , autoload=True , autoload_with=conn)
+            metadata = MetaData()
+            sqlalchemy_table = sqlalchemy.Table(table_name, metadata, autoload=True, autoload_with=conn)
             col_list = []
             for col in sqlalchemy_table.columns:
                 col_list.append(col.name)
-            return col_list
+            return col_list, sqlalchemy_table
 
         async with self._sql_engine.begin() as conn:
-            cols = await conn.run_sync(read_cols)
-            values = [':' + col for col in cols]
-            col_sql = ', '.join(cols)
-            value_sql = ', '.join(values)
-            sql = f'''INSERT INTO {table_name} ({col_sql}) VALUES ({value_sql})'''
-            for index, row in df.iterrows():
-                params = dict()
-                for col in cols:
-                    params[col] = row[col]
-                await conn.execute(text(sql), params)
+            cols, table = await conn.run_sync(read_cols)
+            df = df[cols]
+            records = df.to_dict(orient='records')
+            await conn.execute(insert(table), records)
