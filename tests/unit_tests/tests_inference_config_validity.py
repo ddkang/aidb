620878a59b99f81f8a45f139bd02f9efc103c26c
akash17mittal <akashmittal1798@gmail.com>
2023-09-15 17:43:23 -0500

    ad

--- tests/unit_tests/tests_inference_config_validity.py
+++ tests/unit_tests/tests_inference_config_validity.py
@@ -1,49 +1,34 @@
-import asyncio
-
-from tests.db_utils.db_setup import create_db, setup_db, insert_data_in_tables, clear_all_tables, setup_config_tables
-from tests.inference_service_utils.inference_service_setup import register_inference_services
-from aidb.engine import Engine
 import unittest
 
-DB_URL = "postgresql+asyncpg://postgres@localhost"
+from unittest import IsolatedAsyncioTestCase
+from tests.db_utils.db_setup import create_db, setup_db, clear_all_tables, setup_config_tables
+from tests.inference_service_utils.inference_service_setup import register_inference_services
+from aidb.engine import Engine
+
+# DB_URL = "postgresql+asyncpg://postgres@localhost"
+DB_URL = "sqlite+aiosqlite://"
 
 
-# DB_URL = "sqlite+aiosqlite://"
+class InferenceConfigIntegrityTests(IsolatedAsyncioTestCase):
 
-async def main():
-  data_dir = './tests/data/jackson'
-  # Set up the aidb database
-  aidb_db_fname = 'aidb_test'
-  await create_db(DB_URL, aidb_db_fname)
+  async def test_positive_object_detection(self):
+    data_dir = './tests/data/jackson'
+    # Set up the aidb database
+    aidb_db_fname = 'aidb_test'
+    await create_db(DB_URL, aidb_db_fname)
 
-  tmp_engine = await setup_db(DB_URL, aidb_db_fname, data_dir)
-  await clear_all_tables(tmp_engine)
-  await setup_config_tables(tmp_engine)
-  del tmp_engine
-  # Connect to the aidb database
-  aidb_engine = Engine(
-    f'{DB_URL}/{aidb_db_fname}',
-    debug=False,
-  )
-  register_inference_services(aidb_engine, data_dir)
+    tmp_engine = await setup_db(DB_URL, aidb_db_fname, data_dir)
+    await clear_all_tables(tmp_engine)
+    await setup_config_tables(tmp_engine)
+    del tmp_engine
+    # Connect to the aidb database
+    aidb_engine = Engine(
+      f'{DB_URL}/{aidb_db_fname}',
+      debug=False,
+    )
+    register_inference_services(aidb_engine, data_dir)
+    del aidb_engine
 
 
 if __name__ == '__main__':
-  asyncio.run(main())
-
-# class GraphTests(unittest.TestCase):
-#   async def test_1(self):
-#     g = Graph({"a": ["b", "c"], "b": ["c"]})
-#     self.assertTrue(g.isCyclic())
-#
-#   def test_2(self):
-#     g = Graph({"a": ["b", "c"], "c": ["d"]})
-#     self.assertFalse(g.isCyclic())
-#
-#   def test_3(self):
-#     g = Graph({"a": ["b", "c"], "c": ["d"], "d": ["a"]})
-#     self.assertTrue(g.isCyclic())
-#
-#
-# if __name__ == '__main__':
-#   unittest.main()
+  unittest.main()
