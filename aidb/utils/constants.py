8b048bcc6a2d7bdabd8e5cf70b7be09641006186
akash17mittal <akashmittal1798@gmail.com>
2023-09-15 10:55:44 -0500

    implement hashing

--- aidb/utils/constants.py
+++ aidb/utils/constants.py
@@ -1,3 +1,4 @@
+import hashlib
 from typing import List
 
 
@@ -12,4 +13,12 @@ The schema of the blob metadata table is as follows:
 BLOB_TABLE_NAMES_TABLE = CONFIG_PREFIX + '_blob_tables'
 
 def cache_table_name_from_inputs(service_name: str, columns: List[str]):
-  return f"{CACHE_PREFIX}__{service_name}"
+  cache_table_postfix = ''
+  for idx, column in enumerate(columns):
+    # Special characters are not allowed in table names
+    column = column.replace('.', '__')
+    cache_table_postfix += f'__{idx}_{column}__'
+  # limit to 10 characters to avoid long names in the table namesq
+  hash_length = 10
+  column_input_hash = hashlib.sha1(cache_table_postfix.encode()).hexdigest()[:hash_length]
+  return f"{CACHE_PREFIX}__{service_name}__{column_input_hash}"
