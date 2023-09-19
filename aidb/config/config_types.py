b53baddbf36fc50d5be3c554849479af06e87bf4
Akash Mittal <akashmittal1798@gmail.com>
2023-09-18 19:36:21 -0500

    Test infra setup (#23)

--- aidb/config/config_types.py
+++ aidb/config/config_types.py
@@ -74,3 +74,17 @@ class Table:
       for fk in col.foreign_keys:
         fkeys[col.name] = fk.target_fullname
     return fkeys
+
+
+def python_type_to_sqlalchemy_type(python_type):
+  if python_type == int:
+    return sqlalchemy.Integer
+  elif python_type == float:
+    return sqlalchemy.Float
+  # TODO: think if this is the best way.
+  elif python_type == str or python_type == object:
+    return sqlalchemy.String
+  elif python_type == bool:
+    return sqlalchemy.Boolean
+  else:
+    raise ValueError(f'Unknown python type {python_type}')
---
+++ tests/tests_db_setup.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+
+from unittest import IsolatedAsyncioTestCase
+from tests.db_utils.db_setup import create_db, setup_db, setup_config_tables
+from tests.inference_service_utils.inference_service_setup import register_inference_services
+from aidb.engine import Engine
+
+# DB_URL = "postgresql+asyncpg://postgres@localhost"
+DB_URL = "sqlite+aiosqlite://"
+
+
+class DbSetupTests(IsolatedAsyncioTestCase):
+
+  async def test_positive(self):
+    dirname = os.path.dirname(__file__)
+    data_dir = os.path.join(dirname, 'data/jackson')
+
+    # Set up the aidb database
+    aidb_db_fname = 'aidb_test.sqlite'
+    await create_db(DB_URL, aidb_db_fname)
+
+    tmp_engine = await setup_db(DB_URL, aidb_db_fname, data_dir)
+    await setup_config_tables(tmp_engine)
+    del tmp_engine
+    # Connect to the aidb database
+    aidb_engine = Engine(
+      f'{DB_URL}/{aidb_db_fname}',
+      debug=False,
+    )
+    register_inference_services(aidb_engine, data_dir)
+    del aidb_engine
+
+
+if __name__ == '__main__':
+  unittest.main()
