a51785df28e48f47f85ffced28a83015448c1b65
Chengsong Zhang <continuerevolution@gmail.com>
2023-09-29 22:23:57 -0500

    fix

--- aidb/config/config.py
+++ aidb/config/config.py
@@ -194,18 +194,18 @@ class Config:
       # must exist in the primary key columns of the input tables
       # or the columns that the primary key columns of the input tables refer to.
       for pk_col in input_primary_key_columns:
-        current_table = pk_col.split('.')[0]
-        current_pk_col = pk_col.split('.')[1]
-        raise_exception = True
-        while current_pk_col in self.tables[current_table].foreign_keys:
-          if current_pk_col in self.tables[current_table].foreign_keys:
+        if pk_col not in out_foreign_key_columns and pk_col not in out_primary_key_columns:
+          current_table = pk_col.split('.')[0]
+          current_pk_col = pk_col.split('.')[1]
+          raise_exception = True
+          while current_pk_col in self.tables[current_table].foreign_keys:
             current_pk_col = self.tables[current_table].foreign_keys[current_pk_col]
             current_table = current_pk_col.split('.')[0]
-          if current_pk_col in out_foreign_key_columns or current_pk_col in out_primary_key_columns:
-            raise_exception = False
-            break
-        if raise_exception:
-          raise Exception(f'Primary key column {pk_col} in input table is not refered by output table {output_table}')
+            if current_pk_col in out_foreign_key_columns or current_pk_col in out_primary_key_columns:
+              raise_exception = False
+              break
+          if raise_exception:
+            raise Exception(f'Primary key column {pk_col} in input table is not refered by output table {output_table}')
 
 
   def check_inference_service_validity(self, bound_inference: BoundInferenceService):
