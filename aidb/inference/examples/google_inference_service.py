cdfc329b7532f4d213b1794b356f8a98edceefcf
Chengsong Zhang <continuerevolution@gmail.com>
2023-10-04 23:15:44 -0500

    google vision

--- aidb/inference/examples/google_inference_service.py
+++ aidb/inference/examples/google_inference_service.py
@@ -3,19 +3,23 @@ import requests
 
 from aidb.inference.http_inference_service import HTTPInferenceService
 
-class GoogleVisionImageAnnotate(HTTPInferenceService):
+class GoogleVisionAnnotate(HTTPInferenceService):
   def __init__(
       self,
       token: str,
       project_id: str,
+      infer_type: str='images',
   ):
     '''
     :param str token: The token to use for authentication.
     :param str project_id: The project ID to use for authentication.
     '''
+    assert infer_type in [
+        "images", "files"
+      ], "infer_type must be images"
     super().__init__(
         name='google_vision_image_annotate',
-        url='https://vision.googleapis.com/v1/images:annotate',
+        url=f'https://vision.googleapis.com/v1/{infer_type}:annotate',
         headers={
           'Content-Type': 'application/json; charset=utf-8',
           'Authorization': f'Bearer {token}',
@@ -26,33 +30,57 @@ class GoogleVisionImageAnnotate(HTTPInferenceService):
         is_single=False,
     )
     self._project_id = project_id
+    self.infer_type = infer_type
 
 
   def infer_one(self, input: pd.Series) -> pd.DataFrame:
     input_dict = input.to_dict()
     input_json = {
-      'requests': [
-        {
-          'image': {
-            'content': input_dict['image'],
-          },
-          'features': [
-            {
-              'type': input_dict['feature_type'],
-            },
-          ],
-        },
-      ],
+      'requests': [{
+        'features': input_dict['features'],
+      },],
       'parent': f'projects/{self._project_id}'
     }
+    if 'imageContext' in input_dict:
+      input_json['requests'][0]['imageContext'] = input_dict['imageContext']
+    match self.infer_type:
+      case "files":
+        if 'pages' in input_dict:
+          input_json['requests'][0]['pages'] = input_dict['pages']
+        if 'gcsSource' in input_dict:
+          input_json['requests'][0]['inputConfig'] = {
+            'gcsSource': {
+              'uri': input_dict['gcsSource']
+            }
+          }
+        elif 'content' in input_dict:
+          input_json['requests'][0]['inputConfig'] = {
+            'content': input_dict['content']
+          }
+        else:
+          raise ValueError('input must contain gcsSource or content')
+        if 'mimeType' in input_dict:
+          input_json['requests'][0]['inputConfig']['mimeType'] = input_dict['mimeType']
+      case "images": 
+        if 'image' in input_dict:
+          input_json['requests'][0]['image'] = {
+            'content': input_dict['image']
+          }
+        elif 'gcsImageUri' in input_dict:
+          input_json['requests'][0]['image'] = {
+            'source': {
+              'gcsImageUri': input_dict['gcsImageUri']
+            }
+          }
+        elif 'imageUri' in input_dict:
+          input_json['requests'][0]['image'] = {
+            'source': {
+              'imageUri': input_dict['imageUri']
+            }
+          }
+        else:
+          raise ValueError('input must contain image, gcsImageUri, or imageUri')
     response = requests.post(self._url, json=input_json, headers=self._headers)
     response.raise_for_status()
     response = response.json()
-    response = response['responses'][0]['faceAnnotations'][0]['boundingPoly']['vertices']
-    face_annotation = {
-      'x_min': response[0]['x'],
-      'y_min': response[0]['y'],
-      'x_max': response[2]['x'],
-      'y_max': response[2]['y'],
-    }
-    return pd.DataFrame([face_annotation])
+    return response
