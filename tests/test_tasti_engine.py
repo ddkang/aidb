695ed48975d86354294704e7057fce765d0cd7b5
ttt-77 <1057858192@qq.com>
2023-10-15 00:12:01 -0500

    add limit query test code

---
+++ tests/test_tasti_engine.py
@@ -0,0 +1,69 @@
+from multiprocessing import Process
+import numpy as np
+import os
+import pandas as pd
+import time
+import unittest
+from unittest import IsolatedAsyncioTestCase
+
+
+from tests.inference_service_utils.inference_service_setup import register_inference_services
+from tests.inference_service_utils.http_inference_service_setup import run_server
+from tests.tasti_test.tasti_test import TastiTests, VectorDatabaseType
+from tests.utils import setup_gt_and_aidb_engine
+
+DB_URL = 'sqlite+aiosqlite://'
+
+rep_ids = pd.DataFrame({'blob_id': range(1, 100, 2)})
+rep_ids.set_index('blob_id', inplace=True, drop=True)
+topk_reps = np.ones((1000, 5))
+for i in range(1000):
+  topk_reps[i, :] = np.random.choice(range(1, 100, 2), 5, replace=False)
+topk_dists = np.random.random((1000, 5))
+topk_for_all = pd.DataFrame({'topk_reps': list(topk_reps), 'topk_dists': list(topk_dists)})
+
+
+# DB_URL = 'mysql+aiomysql://aidb:aidb@localhost'
+class FullScanEngineTests(IsolatedAsyncioTestCase):
+
+  async def test_jackson_number_objects(self):
+
+    dirname = os.path.dirname(__file__)
+    data_dir = os.path.join(dirname, 'data/jackson')
+    p = Process(target=run_server, args=[str(data_dir)])
+    p.start()
+    time.sleep(3)
+
+    # vector database configuration
+    index_name = 'tasti'
+    data_size = 1000
+    embedding_dim = 128
+    nb_buckets = 100
+    vector_database_type = VectorDatabaseType.FAISS.value
+
+    tasti_test = TastiTests(index_name, data_size, embedding_dim, nb_buckets, vector_database_type, index_path='./')
+    tasti_index = tasti_test.tasti
+    blob_mapping_table = 'blobs_mapping'
+
+    gt_engine, aidb_engine = await setup_gt_and_aidb_engine(DB_URL, data_dir, tasti_index, blob_mapping_table)
+
+
+    register_inference_services(aidb_engine, data_dir)
+    queries = [
+      '''SELECT * FROM colors02 WHERE frame >= 1000 and colors02.color = 'black' LIMIT 100;''',
+      '''SELECT frame, light_1, light_2 FROM lights01 WHERE light_2 = 'green' LIMIT 100;''',
+      '''SELECT * FROM objects00 WHERE object_name = 'car' OR frame < 100 LIMIT 100;''',
+      '''SELECT * FROM colors02 WHERE frame >= 1000 and colors02.color = 'black';''',
+      '''SELECT frame, light_1, light_2 FROM lights01 WHERE light_2 = 'green';''',
+      '''SELECT * FROM objects00 WHERE object_name = 'car' OR frame < 100;''',
+    ]
+    for aidb_query in queries:
+      print(f'Executing {aidb_query}')
+      aidb_res = aidb_engine.execute(aidb_query)
+      print(f'The length of results is {len(aidb_res)}')
+    del gt_engine
+    p.terminate()
+
+
+if __name__ == '__main__':
+  unittest.main()
