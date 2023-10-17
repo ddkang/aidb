905274ff701d0faa3f91efa3aa3d2813c4c9bbbe
Chengsong Zhang <continuerevolution@gmail.com>
2023-10-17 12:55:10 -0500

    OpenAI/HuggingFace/GoogleVision API (#42)
    * google vision image annotate
    
    * openai audio
    
    * openai
    
    * hf
    
    * google vision
    
    * openai readme
    
    * hfnlp readme
    
    * hf vision
    
    * readme
    
    * google api
    
    * typo
    
    * use flatten/unflatten, remove reimplementation of infer_one
    
    * should work in all cases, but still need more test
    
    * still some problem with hf image
    
    * hf is working
    
    * separator private
    
    * code should be working properly, next step write doc
    
    * doc for openai
    
    * doc hf google
    
    * readme typo
    
    * readme
    
    * token
    
    * default args
    
    * aidb
    
    * two spaces
    
    * cut audio

---
+++ aidb/inference/examples/huggingface_inference_service.py
@@ -0,0 +1,64 @@
+from typing import Dict, Union
+import os
+import requests
+import pandas as pd
+
+from aidb.inference.http_inference_service import HTTPInferenceService
+
+
+class HuggingFaceNLP(HTTPInferenceService):
+  def __init__(
+      self,
+      token: str=None,
+      default_args: Dict[str, Union[str, int]]=None,
+      columns_to_input_keys: Dict[str, Union[str, tuple]]=None,
+      response_keys_to_columns: Dict[Union[str, tuple], str]=None,
+      model: str=None):
+    if token is None:
+      token = os.environ['HF_API_KEY']
+    super().__init__(
+      name='huggingface_nlp',
+      url=f'https://api-inference.huggingface.co/models/{model}',
+      headers={
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': f'Bearer {token}',
+      },
+      default_args=default_args,
+      copy_input=False,
+      batch_supported=False,
+      is_single=False,
+      columns_to_input_keys=columns_to_input_keys,
+      response_keys_to_columns=response_keys_to_columns,
+    )
+
+
+class HuggingFaceVisionAudio(HTTPInferenceService):
+  def __init__(
+      self,
+      token: str=None,
+      default_args: Dict[str, Union[str, int]]=None,
+      response_keys_to_columns: Dict[Union[str, tuple], str]=None,
+      model: str=None):
+    if token is None:
+      token = os.environ['HF_API_KEY']
+    super().__init__(
+      name='huggingface_nlp',
+      url=f'https://api-inference.huggingface.co/models/{model}',
+      headers={
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': f'Bearer {token}',
+      },
+      default_args=default_args,
+      copy_input=False,
+      batch_supported=False,
+      is_single=False,
+      response_keys_to_columns=response_keys_to_columns,
+    )
+
+
+  def request(self, input: pd.Series) -> Dict:
+    filename = input['filename']
+    with open(filename, 'rb') as f:
+      response = requests.post(self._url, data=f, headers=self._headers)
+    response.raise_for_status()
+    return response.json()
---
+++ aidb/inference/examples/openai_inference_service.py
@@ -0,0 +1,93 @@
+from typing import Dict, Union
+
+import os
+
+from aidb.inference.http_inference_service import HTTPInferenceService
+
+
+class OpenAIAudio(HTTPInferenceService):
+  def __init__(
+      self,
+      token: str=None,
+      default_args: Dict[str, Union[str, int]]=None,
+      columns_to_input_keys: Dict[str, Union[str, tuple]]=None,
+      response_keys_to_columns: Dict[Union[str, tuple], str]=None,
+      infer_type: str='transcriptions'):
+    assert infer_type in [
+        "transcriptions",
+        "translations",
+      ], "infer_type must be transcriptions or translations"
+    if token is None:
+      token = os.environ['OPENAI_API_KEY']
+    super().__init__(
+      name='openai_audio',
+      url=f'https://api.openai.com/v1/audio/{infer_type}',
+      headers={
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+      },
+      default_args=default_args,
+      copy_input=False,
+      batch_supported=False,
+      is_single=False,
+      columns_to_input_keys=columns_to_input_keys,
+      response_keys_to_columns=response_keys_to_columns,
+    )
+
+
+class OpenAIImage(HTTPInferenceService):
+  def __init__(
+      self, 
+      token: str=None, 
+      default_args: Dict[str, Union[str, int]]=None,
+      columns_to_input_keys: Dict[str, Union[str, tuple]]=None,
+      response_keys_to_columns: Dict[Union[str, tuple], str]=None,
+      infer_type: str='generations'
+    ):
+    assert infer_type in [
+        "generations",
+        "edits",
+        "variations",
+      ], "infer_type must be generations, edits or variations"
+    if token is None:
+      token = os.environ['OPENAI_API_KEY']
+    super().__init__(
+      name='openai_image',
+      url=f'https://api.openai.com/v1/images/{infer_type}',
+      headers={
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+      },
+      default_args=default_args,
+      copy_input=False,
+      batch_supported=False,
+      is_single=False,
+      columns_to_input_keys=columns_to_input_keys,
+      response_keys_to_columns=response_keys_to_columns,
+    )
+
+
+class OpenAIText(HTTPInferenceService):
+  def __init__(
+      self, 
+      token: str=None,
+      default_args: Dict[str, Union[str, int]]=None,
+      columns_to_input_keys: Dict[str, Union[str, tuple]]=None,
+      response_keys_to_columns: Dict[Union[str, tuple], str]=None,
+    ):
+    if token is None:
+      token = os.environ['OPENAI_API_KEY']
+    super().__init__(
+      name='openai_text',
+      url='https://api.openai.com/v1/chat/completions',
+      headers={
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+      },
+      default_args=default_args,
+      copy_input=False,
+      batch_supported=False,
+      is_single=False,
+      columns_to_input_keys=columns_to_input_keys,
+      response_keys_to_columns=response_keys_to_columns,
+    )
