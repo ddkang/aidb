1a921091201f20f8fd9d6686412e6b173c0e8f6a
Chengsong Zhang <continuerevolution@gmail.com>
2023-10-25 23:54:55 -0500

    multiple infer service

--- config.py
+++ config.py
@@ -12,11 +12,18 @@ inference_engines = [
     name="sentiment_classification",
     columns_to_input_keys={'blobs00.review': 'inputs'},
     response_keys_to_columns={(AIDBListType(), AIDBListType(), 'label'): 'sentiment.label',
-                                (AIDBListType(), AIDBListType(), 'score'): 'sentiment.score'},
+                              (AIDBListType(), AIDBListType(), 'score'): 'sentiment.score'},
     model="LiYuan/amazon-review-sentiment-analysis",
     default_args={"options": {"wait_for_model": True}},
     copy_input=True),
   HuggingFaceNLP(
-      
-  )
+    name="fill_mask",
+    columns_to_input_keys={'inputs': 'inputs'},
+    response_keys_to_columns={(AIDBListType(), 'sequence'): 'sequence',
+                              (AIDBListType(), 'score'): 'score',
+                              (AIDBListType(), 'token'): 'token',
+                              (AIDBListType(), 'token_str'): 'token_str'},
+    model="bert-base-uncased",
+    default_args={"options": {"wait_for_model": True}},
+    copy_input=True)
 ]
