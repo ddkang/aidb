07c07ded575577862a365ec99a203394d68abf0f
Chengsong Zhang <continuerevolution@gmail.com>
2023-10-30 18:56:17 -0500

    test

--- tests/inference_service_utils/inference_service_setup.py
+++ tests/inference_service_utils/inference_service_setup.py
@@ -17,19 +17,17 @@ def register_inference_services(engine: Engine, data_dir: str):
     columns = df.columns
     input_cols = []
     output_cols = []
-    columns_to_input_keys = {}
-    output_keys_to_columns = {}
+    columns_to_input_keys = []
+    output_keys_to_columns = []
     in_col_idx = 0
     out_col_idx = 0
     for col in columns:
       if col.startswith("in__"):
-        columns_to_input_keys[in_col_idx] = col[4:]
+        columns_to_input_keys.append(col[4:])
         input_cols.append(col[4:])
-        in_col_idx += 1
       elif col.startswith("out__"):
-        output_keys_to_columns[(col[5:], AIDBListType())] = out_col_idx
+        output_keys_to_columns.append((col[5:], AIDBListType()))
         output_cols.append(col[5:])
-        out_col_idx += 1
       else:
         raise Exception("Invalid column name, column name should start with in__ or out__")
 
