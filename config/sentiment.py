c5bf7451709bce4c72be32245ddef0901052931a
akash17mittal <akashmittal1798@gmail.com>
2023-11-02 18:28:00 -0500

    refactoring

--- config/sentiment.py
+++ config/sentiment.py
@@ -1,27 +1,32 @@
 from aidb.config.config_types import AIDBListType
 from aidb.inference.examples.huggingface_inference_service import HuggingFaceNLP
 
-
 DB_URL = 'sqlite+aiosqlite://'
 DB_NAME = 'aidb_test_amazon.sqlite'
-DATA_DIR = 'tests/data/amazon'
 HF_KEY = '<hf key>'
 
-
 sentiment_inference_service = HuggingFaceNLP(
-      name="sentiment_classification",
-      token=HF_KEY,
-      columns_to_input_keys=['inputs'],                                     # list index 0 means the first column of the input dataframe, but the actual first column has name "blobs00.review"
-      response_keys_to_columns=[(AIDBListType(), AIDBListType(), 'label'),  # list index 0 means the first column of the output dataframe, but the actual first column has name "sentiment.label" 
-                                (AIDBListType(), AIDBListType(), 'score')], # list index 1 means the second column of the output dataframe, but the actual second column has name "sentiment.score"
-      input_columns_types=[str],
-      output_columns_types=[str, float],
-      model="LiYuan/amazon-review-sentiment-analysis",
-      default_args={"options": {"wait_for_model": True}})
+  name="sentiment_classification",
+  token=HF_KEY,
+  columns_to_input_keys=['inputs'],
+  # list index 0 means the first column of the input dataframe, but the actual first column has name "blobs00.review"
+  response_keys_to_columns=[(AIDBListType(), AIDBListType(), 'label'),
+                            # list index 0 means the first column of the output dataframe, but the actual first column has name "sentiment.label"
+                            (AIDBListType(), AIDBListType(), 'score')],
+  # list index 1 means the second column of the output dataframe, but the actual second column has name "sentiment.score"
+  input_columns_types=[str],
+  output_columns_types=[str, float],
+  model="LiYuan/amazon-review-sentiment-analysis",
+  default_args={"options": {"wait_for_model": True}})
 inference_engines = [
   {
     "service": sentiment_inference_service,
-    "input_col": ("blobs00.review", "blobs00.review_id"), # I leave the bindings as they are for now, but they may change following the change above
+    "input_col": ("blobs00.review", "blobs00.review_id"),
+    # I leave the bindings as they are for now, but they may change following the change above
     "output_col": ("sentiment.label", "sentiment.score", "sentiment.review_id")
   }
 ]
+
+blobs_csv_file = "tests/data/amazon_reviews.csv"
+blob_table_name = "blobs00"
+blobs_keys_columns = ["review_id"]
--- launch.py
+++ launch.py
@@ -1,12 +1,28 @@
 import argparse
+import importlib
+
+import pandas as pd
 
 from aidb_utilities.command_line_setup.command_line_setup import command_line_utility
 from aidb_utilities.aidb_setup.aidb_factory import AIDB
+from aidb_utilities.db_setup.blob_table import BaseTablesSetup
+
+
+def setup_blob_tables(config_path):
+  config = importlib.import_module(config_path)
+  input_blobs = pd.read_csv(config.blobs_csv_file)
+  base_table_setup = BaseTablesSetup(f"{config.DB_URL}/{config.DB_NAME}")
+  base_table_setup.insert_blob_meta_data(config.blob_table_name, input_blobs, config.blobs_keys_columns)
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("--config", type=str)
-
+  parser.add_argument("--setup-blob-table", action='store_true')
   args = parser.parse_args()
-  command_line_utility(AIDB.from_config(args.config))
+
+  if args.setup_blob_table:
+    setup_blob_tables(args.config)
+
+  aidb_engine = AIDB.from_config(args.config)
+  command_line_utility(aidb_engine)
