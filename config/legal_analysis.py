fe759fe969e8873e1436e7e5b9c2d8ec95a64d67
Chengsong Zhang <continuerevolution@gmail.com>
2023-11-08 02:08:58 -0600

    Typo

--- config/legal_analysis.py
+++ config/legal_analysis.py
@@ -12,7 +12,7 @@ ocr = DetectronLocalOCR(
 
 openai_gpt = OpenAIText(
   name="openai_gpt",
-  token=None, # leave it None if you want AIDB to read token from env variable OPENAI_API_KEY. Otherwisr replace None with your own token in str.
+  token=None, # leave it None if you want AIDB to read token from env variable OPENAI_API_KEY. Otherwise replace None with your own token in str.
   columns_to_input_keys=[('messages', AIDBListType(), 'content')],
   response_keys_to_columns=[('choices', AIDBListType(), 'message', 'content')],
   input_columns_types=[str],
