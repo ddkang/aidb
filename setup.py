56594494654c340f0282439b3ab0513d122db33a
ttt-77 <1057858192@qq.com>
2023-11-08 20:34:56 -0600

    Add files for publish

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup(
+setup(
   # Metadata
   name="aidb",
   version="0.0.1",
+  description="",
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
   author="Daniel Kang",
   author_email="daniel.d.kang@gmail.com",
   # Packages
-  packages=setuptools.find_packages(),
+  packages=find_packages(include=["aidb", "aidb.*", "aidb_utilities", "aidb_utilities.*"]),
+  python_requires=">=3.9",
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Information Technology",
+    "Intended Audience :: Legal Industry",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: SQL",
+    "Programming Language :: Python :: 3 :: Only",
+  ],
 )
