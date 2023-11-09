1ca74d11266ce9b02c5c9b9450e97bdd8d3c9057
ttt-77 <43330557+ttt-77@users.noreply.github.com>
2023-11-09 12:38:44 -0600

    Add files for publish (#94)
    * Add files for publish
    
    * Add files for publish
    
    * Update some descriptions
    
    * test without README
    
    * fix license error
    
    * rename
    
    * add required packages
    
    * symlink launch
    
    * update version
    
    ---------
    
    Co-authored-by: Daniel Kang <daniel.d.kang@gmail.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup(
+with open('requirements.txt') as f:
+  required = f.read().splitlines()
+
+setup(
   # Metadata
-  name="aidb",
-  version="0.0.1",
+  name="ai-db",
+  version="0.0.2",
+  description="Analyze your unstructured data",
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
   author="Daniel Kang",
   author_email="daniel.d.kang@gmail.com",
   # Packages
-  packages=setuptools.find_packages(),
+  packages=find_packages(include=["aidb", "aidb.*", "aidb_utilities", "aidb_utilities.*"]),
+  python_requires=">=3.9",
+  install_requires=required,
+  classifiers=[
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: SQL",
+    "Programming Language :: Python :: 3 :: Only",
+  ],
 )
