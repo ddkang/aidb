1309979062ab3f4baa1e418eae2451cad016f5f3
ttt-77 <1057858192@qq.com>
2023-11-08 23:43:49 -0600

    add required packages

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt') as f:
+  required = f.read().splitlines()
+
 setup(
   # Metadata
   name="ai-db",
   version="0.0.1",
   description="Analyze your unstructured data",
-  # long_description=open("README.md").read(),
-  # long_description_content_type="text/markdown",
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
   author="Daniel Kang",
   author_email="daniel.d.kang@gmail.com",
   # Packages
   packages=find_packages(include=["aidb", "aidb.*", "aidb_utilities", "aidb_utilities.*"]),
   python_requires=">=3.9",
+  install_requires=required,
   classifiers=[
     "Intended Audience :: Developers",
     "License :: OSI Approved :: Apache Software License",
