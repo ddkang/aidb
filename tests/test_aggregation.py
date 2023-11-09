a4fd01ff88cfb4623f94c8a830e01e0faca06fc9
Daniel Kang <daniel.d.kang@gmail.com>
2023-11-08 22:40:31 -0600

    bump sqlglot-aidb, % for confidence

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -16,52 +16,52 @@ DB_URL = "sqlite+aiosqlite://"
 queries = [
   (
     'approx_aggregate',
-    '''SELECT SUM(x_min) FROM objects00 WHERE x_min > 1000 ERROR_TARGET 10% CONFIDENCE 95;''',
+    '''SELECT SUM(x_min) FROM objects00 WHERE x_min > 1000 ERROR_TARGET 10% CONFIDENCE 95%;''',
     '''SELECT SUM(x_min) FROM objects00 WHERE x_min > 1000;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT COUNT(x_min) FROM objects00 WHERE x_min > 1000 ERROR_TARGET 10% CONFIDENCE 95;''',
+    '''SELECT COUNT(x_min) FROM objects00 WHERE x_min > 1000 ERROR_TARGET 10% CONFIDENCE 95%;''',
     '''SELECT COUNT(x_min) FROM objects00 WHERE x_min > 1000;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT SUM(x_min) FROM objects00 ERROR_TARGET 10% CONFIDENCE 95;''',
+    '''SELECT SUM(x_min) FROM objects00 ERROR_TARGET 10% CONFIDENCE 95%;''',
     '''SELECT SUM(x_min) FROM objects00;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT SUM(y_min) FROM objects00 ERROR_TARGET 10% CONFIDENCE 95;''',
+    '''SELECT SUM(y_min) FROM objects00 ERROR_TARGET 10% CONFIDENCE 95%;''',
     '''SELECT SUM(y_min) FROM objects00;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT COUNT(x_min) FROM objects00 ERROR_TARGET 10% CONFIDENCE 95;''',
+    '''SELECT COUNT(x_min) FROM objects00 ERROR_TARGET 10% CONFIDENCE 95%;''',
     '''SELECT COUNT(x_min) FROM objects00;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT AVG(x_min) FROM objects00 ERROR_TARGET 5% CONFIDENCE 95;''',
+    '''SELECT AVG(x_min) FROM objects00 ERROR_TARGET 5% CONFIDENCE 95%;''',
     '''SELECT AVG(x_min) FROM objects00;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT AVG(x_max) FROM objects00 ERROR_TARGET 5% CONFIDENCE 95;''',
+    '''SELECT AVG(x_max) FROM objects00 ERROR_TARGET 5% CONFIDENCE 95%;''',
     '''SELECT AVG(x_max) FROM objects00;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT AVG(x_min) FROM objects00 WHERE x_min > 1000 ERROR_TARGET 5% CONFIDENCE 95;''',
+    '''SELECT AVG(x_min) FROM objects00 WHERE x_min > 1000 ERROR_TARGET 5% CONFIDENCE 95%;''',
     '''SELECT AVG(x_min) FROM objects00 WHERE x_min > 1000;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT AVG(x_min) FROM objects00 WHERE y_max < 900 ERROR_TARGET 5% CONFIDENCE 95;''',
+    '''SELECT AVG(x_min) FROM objects00 WHERE y_max < 900 ERROR_TARGET 5% CONFIDENCE 95%;''',
     '''SELECT AVG(x_min) FROM objects00 WHERE y_max < 900;'''
   ),
   (
     'approx_aggregate',
-    '''SELECT AVG(x_min) FROM objects00 WHERE x_min < 700 ERROR_TARGET 5% CONFIDENCE 95;''',
+    '''SELECT AVG(x_min) FROM objects00 WHERE x_min < 700 ERROR_TARGET 5% CONFIDENCE 95%;''',
     '''SELECT AVG(x_min) FROM objects00 WHERE x_min < 700;'''
   ),
 ]
