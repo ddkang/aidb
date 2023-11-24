ebdfdbfb44ed48e7da486e7fcc80591454df6936
ttt-77 <1057858192@qq.com>
2023-11-24 15:00:23 -0600

    support parallel test

--- tests/inference_service_utils/http_inference_service_setup.py
+++ tests/inference_service_utils/http_inference_service_setup.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI, Request
 
 from multiprocessing import Process
 
-def run_server(data_dir: str):
+def run_server(data_dir: str, port=8000):
   app = FastAPI()
 
   inference_dir = f'{data_dir}/inference'
@@ -58,7 +58,7 @@ def run_server(data_dir: str):
 
   # config = Config(app=app, host="127.0.0.1", port=8000)
   # server = Server(config=config)
-  uvicorn.run(app, host="127.0.0.1", port=8000, log_level='warning')
+  uvicorn.run(app, host="127.0.0.1", port=port, log_level='warning')
 
 if __name__=='__main__':
   p = Process(target=run_server, args=["/home/akash/Documents/aidb-new/tests/data/jackson"])
