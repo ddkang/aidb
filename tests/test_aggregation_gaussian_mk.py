a015cb43b9ec9bf37d7d1cd5f3af08265664adb9
Minh Phan <phantuanminh2002@gmail.com>
2023-12-01 02:15:45 -0600

    Change the print into logging in approx_aggregate_engine and its tests

--- tests/test_aggregation_gaussian_mk.py
+++ tests/test_aggregation_gaussian_mk.py
@@ -2,6 +2,7 @@
 This file is used to test aggregation query with multiple blob keys. It draws samples from gaussian distribution,
 and test the accuracy of approximate aggregation engine for Sum/Count/Avg query.
 '''
+import logging
 from multiprocessing import Process
 import numpy as np
 import os
@@ -12,9 +13,19 @@ import unittest
 from unittest import IsolatedAsyncioTestCase
 
 from aidb.query.query import Query
+from aidb.utils.logger import logger
 from tests.inference_service_utils.inference_service_setup import register_inference_services
 from tests.inference_service_utils.http_inference_service_setup import run_server
 from tests.utils import setup_gt_and_aidb_engine
+
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+file_handler = logging.FileHandler('aggregation_gaussian_mk.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 ground_truth_dir = './tests/data/gaussian_mk/ground_truth'
 inference_dir = './tests/data/gaussian_mk/inference'
@@ -93,13 +104,13 @@ class AggeregateEngineTests(IsolatedAsyncioTestCase):
     gt_engine, aidb_engine = await setup_gt_and_aidb_engine(DB_URL, data_dir)
     register_inference_services(aidb_engine, data_dir)
     for query_type, aidb_query, aggregate_query in queries:
-      print(f'Running query {aggregate_query} in ground truth database')
+      logger.info(f'Running query {aggregate_query} in ground truth database')
       async with gt_engine.begin() as conn:
         gt_res = await conn.execute(text(aggregate_query))
         gt_res = gt_res.fetchall()[0]
-      print(f'Running query {aidb_query} in aidb database')
+      logger.info(f'Running query {aidb_query} in aidb database')
       aidb_res = aidb_engine.execute(aidb_query)[0]
-      print(f'aidb_res: {aidb_res}, gt_res: {gt_res}')
+      logger.info(f'aidb_res: {aidb_res}, gt_res: {gt_res}')
       error_target = Query(aidb_query, aidb_engine._config).error_target
       if error_target is None: error_target = 0
       assert self._equality_check(aidb_res, gt_res, error_target)
--- tests/test_exact_aggregation.py
+++ tests/test_exact_aggregation.py
@@ -1,3 +1,4 @@
+import logging
 from multiprocessing import Process
 import os
 from sqlalchemy.sql import text
@@ -6,10 +7,17 @@ import unittest
 from unittest import IsolatedAsyncioTestCase
 
 from aidb.query.query import Query
+from aidb.utils.logger import logger
 from tests.inference_service_utils.inference_service_setup import register_inference_services
 from tests.inference_service_utils.http_inference_service_setup import run_server
 from tests.utils import setup_gt_and_aidb_engine
 
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+file_handler = logging.FileHandler('exact_aggregation.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
 
 DB_URL = "sqlite+aiosqlite://"
 
@@ -43,13 +51,13 @@ class ExactAggeregateTests(IsolatedAsyncioTestCase):
     gt_engine, aidb_engine = await setup_gt_and_aidb_engine(DB_URL, data_dir)
     register_inference_services(aidb_engine, data_dir)
     for query_type, aidb_query, aggregate_query in queries:
-      print(f'Running query {aggregate_query} in ground truth database')
+      logger.info(f'Running query {aggregate_query} in ground truth database')
       async with gt_engine.begin() as conn:
         gt_res = await conn.execute(text(aggregate_query))
         gt_res = gt_res.fetchall()[0]
-      print(f'Running query {aidb_query} in aidb database')
+      logger.info(f'Running query {aidb_query} in aidb database')
       aidb_res = aidb_engine.execute(aidb_query)[0]
-      print(f'aidb_res: {aidb_res}, gt_res: {gt_res}')
+      logger.info(f'aidb_res: {aidb_res}, gt_res: {gt_res}')
       assert aidb_res[0] == gt_res[0]
 
     del gt_engine
