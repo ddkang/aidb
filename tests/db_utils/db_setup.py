5af0410fd0b9364012c51f121f867307e39f7584
Minh Phan <phantuanminh2002@gmail.com>
2023-12-02 20:02:03 -0600

    Add log to database setup

--- tests/db_utils/db_setup.py
+++ tests/db_utils/db_setup.py
@@ -14,9 +14,7 @@ from aidb.utils.logger import logger
 from sqlalchemy.sql import text
 from dataclasses import dataclass
 from typing import Optional
-from tests.utils import setup_test_logger
 
-setup_test_logger('db_setup')
 
 @dataclass
 class ColumnInfo:
