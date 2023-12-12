11d86855cd1ed34501170e0760f8409254265e30
ttt-77 <1057858192@qq.com>
2023-12-13 07:49:43 +0800

    add more tests

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -28,6 +28,31 @@ queries = [
   ),
   (
     'approx_aggregate',
+    '''SELECT SUM(x_min), SUM(y_max), AVG(x_max), COUNT(*) FROM objects00 
+           WHERE y_min > 800 ERROR_TARGET 10% CONFIDENCE 95%;''',
+    '''SELECT SUM(x_min), SUM(y_max), AVG(x_max), COUNT(*) FROM objects00 WHERE y_min > 800;'''
+  ),
+  (
+    'approx_aggregate',
+    '''SELECT SUM(x_min), SUM(y_max), SUM(x_max), SUM(y_min) FROM objects00 
+           WHERE x_min < 1000 ERROR_TARGET 10% CONFIDENCE 95%;''',
+    '''SELECT SUM(x_min), SUM(y_max), SUM(x_max), SUM(y_min) FROM objects00 WHERE x_min < 1000;'''
+  ),
+  (
+    'approx_aggregate',
+    '''SELECT AVG(x_min), SUM(y_max), AVG(x_max), SUM(y_min) FROM objects00 
+           WHERE frame > (SELECT AVG(frame) FROM objects00) ERROR_TARGET 10% CONFIDENCE 95%;''',
+    '''SELECT AVG(x_min), SUM(y_max), AVG(x_max), SUM(y_min) FROM objects00 
+           WHERE frame > (SELECT AVG(frame) FROM objects00);'''
+  ),
+  (
+    'approx_aggregate',
+    '''SELECT COUNT(x_min), SUM(y_max), COUNT(x_max), AVG(y_min) FROM objects00 
+           WHERE x_min > 700 AND y_min > 700 ERROR_TARGET 10% CONFIDENCE 95%;''',
+    '''SELECT COUNT(x_min), SUM(y_max), COUNT(x_max), AVG(y_min) FROM objects00 WHERE x_min > 700 AND y_min > 700;'''
+  ),
+  (
+    'approx_aggregate',
     '''SELECT SUM(x_min) FROM objects00 WHERE x_min > 1000 ERROR_TARGET 10% CONFIDENCE 95%;''',
     '''SELECT SUM(x_min) FROM objects00 WHERE x_min > 1000;'''
   ),
