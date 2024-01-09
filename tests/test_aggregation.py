d3c29addc1d202fb5d322a4cb2d6e724ef8ff653
ttt-77 <1057858192@qq.com>
2024-01-09 21:23:05 +0800

    fix

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -176,9 +176,9 @@ class AggeregateEngineTests(IsolatedAsyncioTestCase):
             count_list[k] += 1
           k+=1
         logger.info(f'Time of runs: {i+1}, Successful count: {count_list}')
-        assert sum(count_list) >= len(count_list) * _NUMBER_OF_RUNS - 1
-        del gt_engine
-        del aidb_engine
+      assert sum(count_list) >= len(count_list) * _NUMBER_OF_RUNS - 1
+      del gt_engine
+      del aidb_engine
     p.terminate()
 
 
