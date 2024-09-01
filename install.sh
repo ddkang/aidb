d75255e528a01b73d479f19943b7b2f86a8380a0
ttt-77 <tengjun2@illinois.edu>
2024-09-01 15:56:52 -0600

    fix

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 # setup miniconda
-mkdir -p /projects/illinois/eng/cs/ddkang/tengjun2/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /projects/illinois/eng/cs/ddkang/tengjun2/miniconda3/miniconda.sh
-bash /projects/illinois/eng/cs/ddkang/tengjun2/miniconda3/miniconda.sh -b -u -p /projects/illinois/eng/cs/ddkang/tengjun2/miniconda3
-rm -rf /projects/illinois/eng/cs/ddkang/tengjun2/miniconda3/miniconda.sh
-export PATH=$/projects/illinois/eng/cs/ddkang/tengjun2/miniconda3/bin:$PATH
-/projects/illinois/eng/cs/ddkang/tengjun2/miniconda3/bin/conda init bash
+mkdir -p ./miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda3/miniconda.sh
+bash ./miniconda3/miniconda.sh -b -u -p ./miniconda3
+rm -rf ./miniconda3/miniconda.sh
+export PATH=$./miniconda3/bin:$PATH
+./miniconda3/bin/conda init bash
 . ~/.bashrc
 
 # setup python environment
--- run_exact.sh
+++ run_exact.sh
@@ -1,5 +1,5 @@
 export DATASET="twitter" # dataset = ['jackson_all', 'law', 'arxiv', 'twitter']
 export PORT=8000
 export AIDB_NUMBER_OF_TEST_RUNS=100
-export BUDGET=5000
+export BUDGET=5000 # The budget for generating proxy scores using the TASTI algorithm
 python3.9 -m tests.vldb_tests.test_approx_select
